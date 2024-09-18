4df3ca269cd9cadf79a7cb91b239d5a54c3dd9ea
jaygithubstore <jayadityasinhvaghela@gmail.com>
2024-09-17 22:32:37 -0500

    Added a Login Page
    Removes the basic info and instead adds a login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<a href="/login">Signin</a>
+<h1>Welcome</h1>
+<p>Login: </p>
+<div style="padding-bottom:10px">
+    <label for="user">Username: </label>
+    <input type="text" name="user" id="user">
+</div>
+<div style="padding-bottom:10px">
+    <label for="pass">Password: </label>
+    <input type="text" name="pass" id="pass">
+</div>
+<div style="padding-bottom:15px">
+    <button>Sign In</button>
+</div>
+<div style="padding-bottom:5px">
+    <button>Sign Up</button>
+</div>
+
