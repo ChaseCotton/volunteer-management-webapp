d61394212dc3969afcba49d2109a4268ee45f7cf
jaygithubstore <jayadityasinhvaghela@gmail.com>
2024-09-20 12:28:48 -0500

    Updated nav bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,13 @@
 <h1>Home I just kept the buttons here for now idk</h1>
+
 <button class="btn">
     <a href="/login">login</a>
 </button>
+
 <button class="btn">
     <a href="/login/userprofile">User Profile Management</a>
+</button>
+
+<button class="btn">
+    <a href="/volunteermatching">Volunteer Matching</a>
 </button>
