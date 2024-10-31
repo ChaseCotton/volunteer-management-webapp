4944a26c0956a5ab439a8b20c12901ffa2ecea2a
jaygithubstore <jayadityasinhvaghela@gmail.com>
2024-10-31 00:30:08 -0500

    Added address and skills proper

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -6,7 +6,12 @@
   let password = '';
   let tempName = '';
   let tempLocation = '';
-  let tempSkills = '';
+  let tempZip = '';
+  let tempState = '';
+  let tempCity = '';
+  let tempSkill1 = '';
+  let tempSkill2 = '';
+  let tempSkill3 = '';
   let tempPreferences = '';
   let tempAvailabilityDates: string[] = [];
   let dateValue = '';
@@ -19,7 +24,7 @@
     } else {
       users[username] = {
         password,
-        profile: { name: tempName, location: tempLocation, skills: tempSkills, preferences: tempPreferences, availability: tempAvailabilityDates }
+        profile: { name: tempName, location: tempLocation, city: tempCity, state: tempState, zip: tempZip, skill1: tempSkill1, skill2: tempSkill2, skill3: tempSkill3, preferences: tempPreferences, availability: tempAvailabilityDates }
       };
       alert("Sign-up successful!");
       resetFields();
@@ -31,7 +36,12 @@
       console.log("User details:", users[loggedInUser].profile);
       tempName = users[loggedInUser].profile.name;
       tempLocation = users[loggedInUser].profile.location;
-      tempSkills = users[loggedInUser].profile.skills;
+      tempCity = users[loggedInUser].profile.city;
+      tempState = users[loggedInUser].profile.state;
+      tempZip = users[loggedInUser].profile.zip;
+      tempSkill1 = users[loggedInUser].profile.skill1;
+      tempSkill2 = users[loggedInUser].profile.skill2;
+      tempSkill3 = users[loggedInUser].profile.skill3;
       tempPreferences = users[loggedInUser].profile.preferences;
       tempAvailabilityDates = [...users[loggedInUser].profile.availability];
     } else {
@@ -43,7 +53,12 @@
     password = '';
     tempName = '';
     tempLocation = '';
-    tempSkills = '';
+    tempCity = '';
+    tempState = '';
+    tempZip = '';
+    tempSkill1 = '';
+    tempSkill2 = '';
+    tempSkill3 = '';
     tempPreferences = '';
     tempAvailabilityDates = [];
     dateValue = '';
@@ -62,7 +77,12 @@
     if (loggedInUser) {
       users[loggedInUser].profile.name = tempName;
       users[loggedInUser].profile.location = tempLocation;
-      users[loggedInUser].profile.skills = tempSkills;
+      users[loggedInUser].profile.city = tempCity;
+      users[loggedInUser].profile.state = tempState;
+      users[loggedInUser].profile.zip = tempZip;
+      users[loggedInUser].profile.skill1 = tempSkill1;
+      users[loggedInUser].profile.skill2 = tempSkill2;
+      users[loggedInUser].profile.skill3 = tempSkill3;
       users[loggedInUser].profile.preferences = tempPreferences;
       users[loggedInUser].profile.availability = [...tempAvailabilityDates];
       alert("Profile updated successfully!");
@@ -98,20 +118,110 @@
 
       <div class="form-control mb-4">
         <label for="location" class="label">
-          <span class="label-text">Location</span>
+          <span class="label-text">Address</span>
         </label>
-        <select id="location" bind:value={tempLocation} class="input input-bordered w-full" required>
-          <option value="New York">New York</option>
+        <input type="text" id="location" bind:value={tempLocation} placeholder="Enter your address" class="input input-bordered w-full" required />
+      </div>
+
+      <div class="form-control mb-4">
+        <label for="city" class="label">
+          <span class="label-text">City</span>
+        </label>
+        <input type="text" id="city" bind:value={tempCity} placeholder="Enter your city" class="input input-bordered w-full" required />
+      </div>
+
+      <div class="form-control mb-4">
+        <label for="state" class="label">
+          <span class="label-text">State</span>
+        </label>
+        <select id="state" bind:value={tempState} class="input input-bordered w-full" required>
+          <option value="Alabama">Alabama</option>
+          <option value="Alaska">Alaska</option>
+          <option value="Arizona">Arizona</option>
+          <option value="Arkansas">Arkansas</option>
           <option value="California">California</option>
+          <option value="Colorado">Colorado</option>
+          <option value="Connecticut">Connecticut</option>
+          <option value="Delaware">Delaware</option>
+          <option value="Florida">Florida</option>
+          <option value="Georgia">Georgia</option>
+          <option value="Hawaii">Hawaii</option>
+          <option value="Idaho">Idaho</option>
+          <option value="Illinois">Illinois</option>
+          <option value="Indiana">Indiana</option>
+          <option value="Iowa">Iowa</option>
+          <option value="Kansas">Kansas</option>
+          <option value="Kentucky">Kentucky</option>
+          <option value="Louisiana">Louisiana</option>
+          <option value="Maine">Maine</option>
+          <option value="Maryland">Maryland</option>
+          <option value="Massachusetts">Massachusetts</option>
+          <option value="Michigan">Michigan</option>
+          <option value="Minnesota">Minnesota</option>
+          <option value="Mississippi">Mississippi</option>
+          <option value="Missouri">Missouri</option>
+          <option value="Montana">Montana</option>
+          <option value="Nebraska">Nebraska</option>
+          <option value="Nevada">Nevada</option>
+          <option value="New Hampshire">New Hampshire</option>
+          <option value="New Jersey">New Jersey</option>
+          <option value="New Mexico">New Mexico</option>
+          <option value="New York">New York</option>
+          <option value="North Carolina">North Carolina</option>
+          <option value="North Dakota">North Dakota</option>
+          <option value="Ohio">Ohio</option>
+          <option value="Oklahoma">Oklahoma</option>
+          <option value="Oregon">Oregon</option>
+          <option value="Pennsylvania">Pennsylvania</option>
+          <option value="Rhode Island">Rhode Island</option>
+          <option value="South Carolina">South Carolina</option>
+          <option value="South Dakota">South Dakota</option>
+          <option value="Tennessee">Tennessee</option>
           <option value="Texas">Texas</option>
+          <option value="Utah">Utah</option>
+          <option value="Vermont">Vermont</option>
+          <option value="Virginia">Virginia</option>
+          <option value="Washington">Washington</option>
+          <option value="West Virginia">West Virginia</option>
+          <option value="Wisconsin">Wisconsin</option>
+          <option value="Wyoming">Wyoming</option>
         </select>
       </div>
 
       <div class="form-control mb-4">
-        <label for="skills" class="label">
-          <span class="label-text">Skills</span>
+        <label for="zip" class="label">
+          <span class="label-text">Zip Code</span>
         </label>
-        <select id="skills" bind:value={tempSkills} class="input input-bordered w-full" required>
+        <input type="number" id="zip" bind:value={tempZip} placeholder="Enter your zip code" class="input input-bordered w-full" required />
+      </div>
+
+      <div class="form-control mb-4">
+        <label for="skill1" class="label">
+          <span class="label-text">Skill 1</span>
+        </label>
+        <select id="skill1" bind:value={tempSkill1} class="input input-bordered w-full">
+          <option value="Communication">Communication</option>
+          <option value="Teamwork">Teamwork</option>
+          <option value="Problem-solving">Problem-solving</option>
+        </select>
+      </div>
+
+      <div class="form-control mb-4">
+        <label for="skill2" class="label">
+          <span class="label-text">Skill 2</span>
+        </label>
+        <select id="skill2" bind:value={tempSkill2} class="input input-bordered w-full">
+          <option value="Communication">Communication</option>
+          <option value="Teamwork">Teamwork</option>
+          <option value="Problem-solving">Problem-solving</option>
+        </select>
+      </div>
+
+      <div class="form-control mb-4">
+        <label for="skill3" class="label">
+          <span class="label-text">Skill 3</span>
+        </label>
+        <select id="skill3" bind:value={tempSkill3} class="input input-bordered w-full">
           <option value="Communication">Communication</option>
           <option value="Teamwork">Teamwork</option>
           <option value="Problem-solving">Problem-solving</option>
@@ -145,17 +255,111 @@
       </div>
 
       <div class="form-control mb-4">
-        <label for="location" class="label"><strong>Location:</strong></label>
-        <select bind:value={tempLocation} class="input input-bordered w-full">
-          <option value="New York">New York</option>
+        <label for="location" class="label">
+          <span class="label-text">Address</span>
+        </label>
+        <input type="text" id="location" bind:value={tempLocation} placeholder="Enter your address" class="input input-bordered w-full" required />
+      </div>
+
+      <div class="form-control mb-4">
+        <label for="city" class="label">
+          <span class="label-text">City</span>
+        </label>
+        <input type="text" id="city" bind:value={tempCity} placeholder="Enter your city" class="input input-bordered w-full" required />
+      </div>
+
+      <div class="form-control mb-4">
+        <label for="state" class="label">
+          <span class="label-text">State</span>
+        </label>
+        <select id="state" bind:value={tempState} class="input input-bordered w-full" required>
+          <option value="Alabama">Alabama</option>
+          <option value="Alaska">Alaska</option>
+          <option value="Arizona">Arizona</option>
+          <option value="Arkansas">Arkansas</option>
           <option value="California">California</option>
+          <option value="Colorado">Colorado</option>
+          <option value="Connecticut">Connecticut</option>
+          <option value="Delaware">Delaware</option>
+          <option value="Florida">Florida</option>
+          <option value="Georgia">Georgia</option>
+          <option value="Hawaii">Hawaii</option>
+          <option value="Idaho">Idaho</option>
+          <option value="Illinois">Illinois</option>
+          <option value="Indiana">Indiana</option>
+          <option value="Iowa">Iowa</option>
+          <option value="Kansas">Kansas</option>
+          <option value="Kentucky">Kentucky</option>
+          <option value="Louisiana">Louisiana</option>
+          <option value="Maine">Maine</option>
+          <option value="Maryland">Maryland</option>
+          <option value="Massachusetts">Massachusetts</option>
+          <option value="Michigan">Michigan</option>
+          <option value="Minnesota">Minnesota</option>
+          <option value="Mississippi">Mississippi</option>
+          <option value="Missouri">Missouri</option>
+          <option value="Montana">Montana</option>
+          <option value="Nebraska">Nebraska</option>
+          <option value="Nevada">Nevada</option>
+          <option value="New Hampshire">New Hampshire</option>
+          <option value="New Jersey">New Jersey</option>
+          <option value="New Mexico">New Mexico</option>
+          <option value="New York">New York</option>
+          <option value="North Carolina">North Carolina</option>
+          <option value="North Dakota">North Dakota</option>
+          <option value="Ohio">Ohio</option>
+          <option value="Oklahoma">Oklahoma</option>
+          <option value="Oregon">Oregon</option>
+          <option value="Pennsylvania">Pennsylvania</option>
+          <option value="Rhode Island">Rhode Island</option>
+          <option value="South Carolina">South Carolina</option>
+          <option value="South Dakota">South Dakota</option>
+          <option value="Tennessee">Tennessee</option>
           <option value="Texas">Texas</option>
+          <option value="Utah">Utah</option>
+          <option value="Vermont">Vermont</option>
+          <option value="Virginia">Virginia</option>
+          <option value="Washington">Washington</option>
+          <option value="West Virginia">West Virginia</option>
+          <option value="Wisconsin">Wisconsin</option>
+          <option value="Wyoming">Wyoming</option>
         </select>
       </div>
 
       <div class="form-control mb-4">
-        <label for="skills" class="label"><strong>Skills:</strong></label>
-        <select bind:value={tempSkills} class="input input-bordered w-full">
+        <label for="zip" class="label">
+          <span class="label-text">Zip Code</span>
+        </label>
+        <input type="number" id="zip" bind:value={tempZip} placeholder="Enter your zip code" class="input input-bordered w-full" required />
+      </div>
+
+      <div class="form-control mb-4">
+        <label for="skill1" class="label">
+          <span class="label-text">Skill 1</span>
+        </label>
+        <select id="skill1" bind:value={tempSkill1} class="input input-bordered w-full">
+          <option value="Communication">Communication</option>
+          <option value="Teamwork">Teamwork</option>
+          <option value="Problem-solving">Problem-solving</option>
+        </select>
+      </div>
+
+      <div class="form-control mb-4">
+        <label for="skill2" class="label">
+          <span class="label-text">Skill 2</span>
+        </label>
+        <select id="skill2" bind:value={tempSkill2} class="input input-bordered w-full">
+          <option value="Communication">Communication</option>
+          <option value="Teamwork">Teamwork</option>
+          <option value="Problem-solving">Problem-solving</option>
+        </select>
+      </div>
+
+      <div class="form-control mb-4">
+        <label for="skill3" class="label">
+          <span class="label-text">Skill 3</span>
+        </label>
+        <select id="skill3" bind:value={tempSkill3} class="input input-bordered w-full">
           <option value="Communication">Communication</option>
           <option value="Teamwork">Teamwork</option>
           <option value="Problem-solving">Problem-solving</option>
